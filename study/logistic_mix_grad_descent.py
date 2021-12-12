88e9a2dcb291b3bf0f294dd3cff18d56bc8c72c2
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-12-11 19:51:24 -0800

    Study of logistic pixel colors.

--- study/logistic_mix_grad_descent.py
+++ study/logistic_mix_grad_descent.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.distributions as dst
 from torch.optim import Adam
 from matplotlib import pyplot as plt
@@ -7,12 +8,15 @@ from torchvision.datasets import CIFAR10
 from torchvision.transforms import ToTensor
 
 
-def logistic_dist(loc, scale):
-    return dst.TransformedDistribution(
-        base_distribution=dst.Uniform(low=torch.zeros(loc.shape), high=torch.ones(loc.shape) * 255),
-        transforms=[dst.AffineTransform(loc=0, scale=1 / 255.0), dst.SigmoidTransform().inv,
+def mix_logistic_dist(pi, loc, scale):
+    pi, loc, scale = torch.log_softmax(pi, -1), loc, F.softplus(scale)
+    pi_dist = dst.Categorical(logits=pi)
+    logistic_dist =  dst.TransformedDistribution(
+        base_distribution=dst.Uniform(0., 1.),
+        transforms=[dst.SigmoidTransform().inv,
                     dst.AffineTransform(loc=loc, scale=scale)]
     )
+    return dst.MixtureSameFamily(pi_dist, logistic_dist)
 
 
 fig, axes = plt.subplots(1, 2)
@@ -20,19 +24,18 @@ img_x, img_y = axes
 
 ds = CIFAR10(root='~/data', transform=ToTensor())
 img, label = ds[70]
-img = img * 255
-img_x.imshow((img.permute(1, 2, 0)).to(dtype=torch.uint8))
+img_x.imshow((img.permute(1, 2, 0)))
 
 
 class ImageGen(nn.Module):
     def __init__(self, img, n_mix=10):
         super().__init__()
         self.pi = nn.Parameter(torch.rand((*img.shape, n_mix)))
-        self.loc = nn.Parameter(torch.rand((*img.shape, n_mix)) * 255.0)
-        self.scale = nn.Parameter(torch.rand((*img.shape, n_mix)) * 40.0)
+        self.loc = nn.Parameter(torch.rand((*img.shape, n_mix)))
+        self.scale = nn.Parameter(torch.rand((*img.shape, n_mix)))
 
     def forward(self):
-        return torch.log_softmax(self.pi, dim=-1), logistic_dist(self.loc, self.scale)
+        return mix_logistic_dist(self.pi, self.loc, self.scale)
 
 
 model = ImageGen(img)
@@ -41,16 +44,15 @@ optim = Adam(params=model.parameters(), lr=1e-2)
 
 for epoch in range(1000000):
     optim.zero_grad()
-    pi, dist = model()
-    lp = dist.log_prob(img.unsqueeze(-1))
-    loss = -(pi + lp).mean()
+    dist = model()
+    lp = dist.log_prob(img)
+    loss = -lp.mean()
     loss.backward()
     optim.step()
     print(loss.item())
 
-    if epoch % 1000 == 0:
+    if epoch % 10 == 0:
         y = dist.sample()
-        y = torch.sum(y * pi.exp(), dim=-1)
         img_y.clear()
-        img_y.imshow(y.to(dtype=torch.uint8).permute(1, 2, 0))
+        img_y.imshow(y.permute(1, 2, 0).clamp(0, 1))
         plt.pause(0.05)
